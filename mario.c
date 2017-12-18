a34a3b67089a1eec9311629d1b97a3549ec6cb50
its-sonam <its-sonam@users.noreply.github.com>
2017-12-18 16:46:36 +0000

    20171218T164628Z

--- mario.c
+++ mario.c
@@ -4,8 +4,11 @@ int main(void)
 {
     printf("Height: ");
    int height=get_int(); int l=1;
-
-  for(int i=0;i<height&&height<23;i++)
+   if(height>23||height<1)
+   {
+       height=get_int();
+   }
+  for(int i=0;i<height&&height<23&&height>0;i++)
       {
           for(int j=height;j>i;j--)
           {
